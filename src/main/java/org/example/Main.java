7c124d9cec633ea95f898eee0dee717f80924583
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-12-08 22:24:14 +0300

    Final HomeWork Serializable data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,32 +4,24 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/**
+ * Написать класс с двумя методами:
+ * 1. принимает объекты, имплементирующие интерфейс serializable, и сохраняющие их в файл.
+ * Название файл - class.getName() + "_" + UUID.randomUUID().toString()
+ * 2. принимает строку вида class.getName() + "_" + UUID.randomUUID().toString()
+ * и загружает объект из файла и удаляет этот файл.
+ * Что делать в ситуациях, когда файла нет или в нем лежит некорректные данные - подумать самостоятельно.
+ */
 public class Main {
     public static void main(String[] args) {
-        /**
-         * Написать класс с двумя методами:
-         * 1. принимает объекты, имплементирующие интерфейс serializable, и сохраняющие их в файл. Название файл - class.getName() + "_" + UUID.randomUUID().toString()
-         *
-         *
-         *
-         *
-         * 2. принимает строку вида class.getName() + "_" + UUID.randomUUID().toString() и загружает объект из файла и удаляет этот файл.
-         *
-         * Что делать в ситуациях, когда файла нет или в нем лежит некорректные данные - подумать самостоятельно.
-         */
-
-//            1. принимает объекты, имплементирующие интерфейс serializable, и сохраняющие их в файл. Название файл - class.getName() + "_" + UUID.randomUUID().toString()
-//            2. принимает строку вида class.getName() + "_" + UUID.randomUUID().toString() и загружает объект из файла и удаляет этот файл.
-//
         String fileName = MyData.class.getName() + "_" + UUID.randomUUID().toString();
         try {
             File file = new File(fileName);
-            file.createNewFile();
-            System.out.println("File created: " + file.getAbsolutePath());
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getAbsolutePath());
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e.getMessage(), e);
+            e.printStackTrace();
         }
 
         MyData myData = new MyData("Chirkov");
@@ -38,11 +30,18 @@ public class Main {
         MyIOController<MyData> myIOController = new MyIOController<>();
 
         System.out.println("Object created: \n" + myData);
-        myIOController.saveDataFromFile(myData, fileName);
-
-        // 2. принимает строку вида class.getName() + "_" + UUID.randomUUID().toString() и загружает объект из файла и удаляет этот файл.
-        MyData loadedData = myIOController.loadDataFromFile(fileName);
-        System.out.println("Object loaded: ");
-        System.out.println(loadedData);
+        try {
+            myIOController.saveDataFromFile(myData, fileName);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        MyData loadedData;
+        try {
+            loadedData = myIOController.loadDataFromFile(fileName);
+            System.out.println("Object loaded: ");
+            System.out.println(loadedData);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 }
